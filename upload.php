b85841f41b23a64bd9f192f7cb9aa18c2458d8bf
maty4215 <96356426+maty4215@users.noreply.github.com>
2023-02-13 21:15:07 +0100

    Add files via upload

---
+++ upload.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="icon" href="images/dsdsdsds.png">
+    <link rel="stylesheet" href="praxe.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nahrání obrázku</title>
+</head>
+<body>
+    <h1 id="nadpis">Nahrání obrázku na stránku</h1>
+
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        <h2>Vyber obrázek</h2>
+        <input type="file" name="fileToUpload" class="file">
+        <input type="submit" value="Upload" name="submit" class="button">
+    </form>
+
+<?php
+
+    if (isset($_POST['submit'])) {
+        $target_file = $_FILES['fileToUpload']['tmp_name'];
+        $image_info = getimagesize($target_file);
+        $image_type = $image_info[2];
+
+        }
+
+        echo '<img src="data:' . $image_info['mime'] . ';base64,' . base64_encode(file_get_contents($target_file)) . '" / height="500">';
+    
+?>
+
+</body>
+</html>
